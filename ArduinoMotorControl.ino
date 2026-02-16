288123531180e113b00867e9b9bbc6481aa074c1
JPlane6 <ayushsharma2146@gmail.com>
2026-02-16 10:36:39 -0500

    Used the Adafruit Library and made the move method(can handle moving both forward and backward)

--- ArduinoMotorControl.ino
+++ ArduinoMotorControl.ino
@@ -1,28 +1,69 @@
-int motor1Forward = 2; // connected internally to shield
-int motor1Backward = 3;
+#include "AFMotor_R4.h" // Import Adafruit Motor Driver Library
+
+AF_DCMotor motor1(1);  // Create motor on M1
+AF_DCMotor motor2(2);  // Create motor on M2
+AF_DCMotor motor3(3);  // Create motor on M3
+AF_DCMotor motor4(4);  // Create motor on M4
+
+//-------------------------------------|
+// Protoype Methods
+void move(uint8_t direction ,uint16_t seconds, uint8_t speed, AF_DCMotor &motor);
+//-------------------------------------|
+
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(motor1Forward, OUTPUT);
-  pinMode(motor1Backward, OUTPUT);
+
+//-------------------------------------|
+// Set initial speed and Brake Motors
+motor1.setSpeed(0);
+motor1.run(BRAKE);
+
+motor2.setSpeed(0);
+motor2.run(BRAKE);
+
+motor3.setSpeed(0);
+motor3.run(BRAKE);
+
+motor4.setSpeed(0);
+motor4.run(BRAKE);
+//-------------------------------------|
+
 }
 
 void loop() {
-  if (Serial.available()) {
-    char cmd = Serial.read();
-    switch(cmd) {
-      case 'f': // forward
-        digitalWrite(motor1Forward, HIGH);
-        digitalWrite(motor1Backward, LOW);
-        break;
-      case 'b': // backward
-        digitalWrite(motor1Forward, LOW);
-        digitalWrite(motor1Backward, HIGH);
-        break;
-      case 's': // stop
-        digitalWrite(motor1Forward, LOW);
-        digitalWrite(motor1Backward, LOW);
-        break;
-    }
+  
+  move(FORWARD, 1, 200, motor1);  // Test the function
+  move(BACKWARD, 1, 200, motor1);
+  while(true){} // Park the Robot here after Test runs
+
+}
+// Method for smoothly moving forward for parameters seconds, speed, and which motor is affected
+void move(uint8_t direction ,uint16_t seconds, uint8_t speed, AF_DCMotor &motor) {
+
+  motor.run(direction);
+
+  // Smoothly scale the motor up to speed specified
+  for (uint8_t i = 0; i < speed; i++) {
+    motor.setSpeed(i);
+    delay(10);
   }
+
+  // To avoid an infinite loop due to i being an unsigned integer(they loop around) make max speed manually 255 if needed
+  if (speed == 255) {
+    motor.setSpeed(255);
+  }
+
+  delay(seconds * 1000);  // let the motor run for a specified amount of time(delay() parameter is in milliseconds)
+
+  // Smoothly scale down the motor to stop it
+  for (uint8_t i = speed; i > 0; i--) {
+    motor.setSpeed(i);
+    delay(10);
+  }
+
+  motor.setSpeed(0);  // To avoid an infinite loop manually set speed to 0
+  
+  motor.run(RELEASE); // Cut power to motor after it is done running
+  delay(10);
+
 }
