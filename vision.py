e94bbcfc22b885561f44fa2951a7ccc61dcb1508
JPlane6 <ayushsharma2146@gmail.com>
2026-03-01 20:37:22 -0500

    Started working on Basic Computer Vision
    Created a structure for Computer Vision and made a venv to keep things safe. Created a basic test file to make usre that Computer Vision and Offline Text to Speech Work.

--- vision.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-def detect_objects(image_path):
-    model = YOLO("yolov8n.pt")  # nano model, fastest
-    img = cv2.imread(image_path)
-    results = model.predict(img)
-    detected_classes = [model.names[int(box.cls)] for box in results[0].boxes]
-    return detected_classes
